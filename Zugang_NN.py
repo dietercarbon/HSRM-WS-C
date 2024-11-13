ef80187b0fb735a7456262109afcd825389d3f6a
Dieter Carbon <dieter.carbon@comidio.de>
2024-11-13 14:30:47 +0100

    Add files via upload

---
+++ Zugang_NN.py
@@ -0,0 +1,17 @@
+def wlanSSID():
+    return "FRITZBox_neu"
+#    return "TP-Link"
+#    return "TP-Link"
+#    return "TP-Link"
+
+def wlanPW():
+    return "12345678901234567890"
+#    return "picobello"
+#    return "picobello"
+#    return "picobello"
+
+def IP_MQTT_broker():
+    return "192.168.178.65"
+#    return "192.168.0.101"
+#    return "192.168.0.101"
+
